c9afe0293955cc42fc134da08c5421757f064779
Wannxing <wanxing0122@163.com>
2022-05-29 13:34:59 +0800

    main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'AI-Lab',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
