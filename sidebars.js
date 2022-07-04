9b64837f3ea72f07203ef1afd51530fb3562a9b6
Wannxing <wanxing0122@163.com>
2022-07-04 23:31:25 +0800

    update exam

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
   someSidebar: {
     // Docusaurus: ['doc1', 'doc2', 'doc3','doc4'],
     // Features: ['mdx'],
+    考核: ['exam1'],
     学习任务: ['task1'],
     ROS教程: ['ros-install'],
   },
