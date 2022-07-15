344881275f1c8e46c92b741fa61f222306e8d340
Wannxing <wanxing0122@163.com>
2022-07-15 17:00:55 +0800

    update prac

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,7 @@ module.exports = {
     // Docusaurus: ['doc1', 'doc2', 'doc3','doc4'],
     // Features: ['mdx'],
     考核: ['exam1'],
+    实践:['prac1'],
     学习任务: ['task1'],
     ROS教程: ['ros-install'],
   },
